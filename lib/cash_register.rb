2fadc2c3d66df4d8139a9359acd430496c2fdc8b
John Souza <souzajohnlee@gmail.com>
2020-03-10 22:43:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
-
+class CashRegister
+  
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @transactions = []
+  end
+  
+  def total
+    @total
+  end
+  
+  def add_item(title, price, quanitity=1)
+    total += (price * quantity)
+    @items.push(title)
+    @transactions.push(price*quantity)
+  end    
+  
+  def apply_discount
+    if discount == 0
+      return "No discount"
+    else 
+      @total = @total * (discount/100)
+      "discount successful"
+    end
+  end 
+  
+  def void_last_transaction
+    @items = @items.pop
+    @total -= @transactions[-1]
+    @transactions = @transactions.pop
+    @total
+  end
+    
+end
+  
