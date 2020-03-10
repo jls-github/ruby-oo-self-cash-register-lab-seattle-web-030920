55bd1729a82cb09d04e00c98ff09049bc6b28325
John Souza <souzajohnlee@gmail.com>
2020-03-10 22:58:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,7 +21,7 @@ class CashRegister
   
   def apply_discount
     if @discount == 0
-      return "No discount"
+      "There is no discount to apply"
     else 
       @total = @total * (100 - @discount) / 100
       "After the discount, the total comes to $#{total}."
